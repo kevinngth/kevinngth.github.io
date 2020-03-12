a7d37124ae7b5595deaf6623f36ab29270303100
kevinngth <kevin.ng.th@gmail.com>
2020-03-12 22:36:24 +0800

    Updates

---
+++ precache-manifest.cadc73fd09be7047094b4d721468f0c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d34891bac0a1eeeef88fe667e4ab5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f21fade385cd0db1b0f8",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9b0ab055e799e6ff2ce8",
+    "url": "/static/css/main.38805be4.chunk.css"
+  },
+  {
+    "revision": "f21fade385cd0db1b0f8",
+    "url": "/static/js/2.d305448b.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.d305448b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0ab055e799e6ff2ce8",
+    "url": "/static/js/main.3bc6b7ec.chunk.js"
+  },
+  {
+    "revision": "8c2697bf92e69aac4f4b",
+    "url": "/static/js/runtime-main.550f578c.js"
+  }
+]);
