7893c6e9dc9574a8b39d873810460d3a497ba344
kevinngth <kevin.ng.th@gmail.com>
2020-03-12 22:19:08 +0800

    updated files

--- src/Components/MyPortfolio/MyPortfolio.jsx
+++ src/Components/MyPortfolio/MyPortfolio.jsx
@@ -1,19 +1,54 @@
-import React from "react";
-import './MyPortfolio.scss';
+import React, { useState } from "react";
+import "./MyPortfolio.scss";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import PORTFOLIO from './portfolio';
-import Image from 'react-bootstrap/Image'
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from 'react-bootstrap/Button';
+import Image from "react-bootstrap/Image";
+import PORTFOLIO from "./portfolio";
 
 function MyPortfolio() {
+    const [portfolio, setPortfolio] = useState(PORTFOLIO);
+    const slideProjects = direction => {
+        let item;
+        switch (direction) {
+            case "upward":
+                item = portfolio.pop();
+                setPortfolio([item, ...portfolio]);
+                break;
+            case "downward":
+                item = portfolio.shift();
+                setPortfolio([...portfolio, item]);
+                break;
+            default:
+                console.log("Something went wrong");
+        }
+    };
     return (
         <Row>
-            <Col>
-                <Row classNames="card-top"><Image src={PORTFOLIO[0].img_src} fluid /></Row>
-                <Row classNames="card-mid"><Image src={PORTFOLIO[1].img_src} fluid /></Row>
-                <Row classNames="card-bottom"><Image src={PORTFOLIO[2].img_src} fluid /></Row>
+            <Col md={6} className="d-flex flex-column justify-content-center align-items-center">
+                <Row className="slide-top" onClick={() => slideProjects("upward")}>
+                    <Image src={portfolio[0].img_src} fluid />
+                </Row>
+                <Row className="slide-mid">
+                    <Image style={{ zIndex: "2" }} src={portfolio[1].img_src} fluid/>
+                </Row>
+                <Row className="slide-bottom" onClick={() => slideProjects("downward")}>
+                    <Image src={portfolio[2].img_src} fluid />
+                </Row>
+            </Col>
+            <Col className="d-flex justify-content-center align-items-center">
+                <div>
+                    <h3>{portfolio[1].title}</h3>
+                    <p>{portfolio[1].text}</p>
+                    <ButtonGroup aria-label="Basic example">
+                        <Button href={portfolio[1].app_link} variant='info'>App</Button>
+                        <Button href={portfolio[1].repo_link} variant='info'>Repo</Button>
+                    </ButtonGroup>
+                </div>
             </Col>
         </Row>
-    )}
+    );
+}
 
 export default MyPortfolio;
